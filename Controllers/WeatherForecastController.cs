cf0b14ca136cc636f2cfc3fb5f3a0aed9e5991c2
AlirezaNoor <alirezang7575@gmail.com>
2024-05-20 10:16:24 -0400

    commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tets.Controllers;
+
+[ApiController]
+ 
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+ 
+    [HttpGet]
+    [Authorize]
+    [Route("test")]
+    public IActionResult GetAll()
+    {
+        return Ok(new { message = "You are authenticated!" });
+    }
+}
